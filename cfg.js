ba360c3bb6d2978b0dd8c9ef49e72523531c33d9
Li Yong <kaven276@vip.sina.com>
2015-10-19 09:20:05 +0800

    server.js put servlet first, rm ReqBase,adjust

--- cfg.js
+++ cfg.js
@@ -6,7 +6,6 @@ module.exports = {
   http_port: parseInt(process.argv[3] || 8888),
   dispatcher_addr: (process.argv[2] || '1522').split(':'),
   demo_dbu: 'demo',
-  static_url: '/',
   static_root: __dirname + '/static',
   upload_dir: __dirname + '/upload',
   template_dir: __dirname + '/static/template',
